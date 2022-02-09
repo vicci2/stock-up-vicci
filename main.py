0c89f3d59681abd17af93915b531dd53d206a414
Vicci2 <vicci2regia@gmail.com>
2022-02-09 19:18:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,139 @@
+import psycopg2
+from flask import Flask, flash, redirect, render_template, request, url_for,session
+
+
+try:
+    conn = psycopg2.connect("dbname='duka' user='postgres' host='localhost' password='vicciSQL'")
+    # conn = psycopg2.connect("dbname='d4augsl57bdont' user='epbnudknwnktpp' port='5432' host='ec2-34-199-15-136.compute-1.amazonaws.com' password='863b87226bd0eafa17748cc14d4bdcb0af464d40c35796f8bf38cce8dc378153'")
+except:
+    print ("I am unable to connect to the  Vicci database")
+
+app =Flask(__name__)
+app.secret_key="123secrete kye"
+
+@app.route('/')
+def ims():
+    return render_template("viccistockims.html")
+
+@app.route('/home')
+def home():
+    return render_template("viccistockhome.html")
+
+@app.route('/dashboard')
+def dash():
+    cur=conn.cursor()
+    
+    cur.execute("SELECT  extract(year from sl.created_at) || '-' || extract(month from sl.created_at) || '-' || extract(day from sl.created_at)as tarehe,sum((pr.sp-pr.bp)* sl.quantity) as totalprofit,sum(sl.quantity)as totalquantity FROM public.sales as sl join products as pr on pr.id=sl.product_id  group by sl.created_at order by sl.created_at")
+    
+    mbp=cur.fetchall()
+    print(mbp)
+    label=[]
+    data=[]   
+    for i in mbp:
+        label.append(i[0])
+        data.append(int(i[1]))
+    print("abxb",data,"uybce",label)
+
+    return render_template("viccistockdash.html",label=label,data=data)
+
+@app.route('/inventory', methods=["GET","POST"])
+def invent():
+    cur=conn.cursor()
+  
+    cur.execute("SELECT * from products")
+    
+    mbp= cur.fetchall()
+    print(mbp)
+    # invent=[(1,"Vicci","Omo",500,900,20,"Product"),(2,"Mani","Sunlight",342,723,12,"Product"),(3,"Shameful","Aerial",928,7093,2,"service")]
+    return render_template("viccistockinvetry.html",invtory=mbp)
+
+@app.route('/add_item',methods=["POST"])
+def adder():
+    cur=conn.cursor()
+    if request.method == "POST":        
+        name= request.form["name"]
+        bp= request.form["bp"]
+        sp= request.form["sp"]
+        serial_no=request.form["serial"] 
+
+        query="INSERT INTO products(name, bp, sp, serial_no) VALUES (%s, %s, %s, %s);"
+        row=(name,bp,sp,serial_no)
+        cur.execute(query,row)
+        conn.commit()
+        flash('Product Successfully Added') 
+        return redirect(url_for('invent')) 
+
+@app.route('/Make_Sale',methods=["POST"])
+def saler():
+    cur=conn.cursor()  
+    id= request.form["product_id"]  
+    quantity= request.form["quantity"]
+    created_at="NOW()"
+    query="INSERT INTO public.sales (product_id,quantity,created_at) VALUES (%s,%s,%s);"
+    row=(id,quantity,created_at)
+    cur.execute(query,row)
+    conn.commit()
+    flash('Purchace Successful') 
+    return redirect(url_for('invent')) 
+
+@app.route('/edit',methods=["POST"])
+def editor():
+    cur=conn.cursor()
+    if request.method == "POST":        
+        name= request.form["name"]
+        bp= request.form["bp"]
+        sp= request.form["sp"]
+        serial_no=request.form["serial"] 
+        id=request.form["id"]        
+
+        query='UPDATE public.products SET name=%s, bp=%s, sp=%s, serial_no=%s WHERE id=%s;'
+        row=(name,bp,sp,serial_no,id)
+        cur.execute(query,row)
+        conn.commit()
+        flash('Product Successfully Edited') 
+        return redirect(url_for('invent'))
+
+@app.route('/stock')
+def stock():    
+    cur=conn.cursor()
+    cur.execute(" SELECT id, product_name, quantity, bp*quantity as ttlcoast, date FROM public.stock; ")      
+    stock=cur.fetchall()
+    print(stock)
+    return render_template("viccistockstock.html",stock=stock)
+
+@app.route('/add_stock',methods=["POST"])
+def stockup():
+    cur=conn.cursor()
+    if request.method == "POST":        
+        name= request.form["product_name"]
+        quantity=request.form["quantity"]
+        bp= request.form["bp"]         
+        created_at="NOW()"
+        query="INSERT INTO public.stock (product_name, quantity, bp,date) VALUES (%s,%s,%s,%s);"
+        row=(name,quantity,bp,created_at)
+        cur.execute(query,row)
+        conn.commit()
+        flash('Product Successfully Added') 
+        return redirect(url_for('stock')) 
+
+@app.route('/sales')
+def sale():
+    cur=conn.cursor()
+
+    cur.execute("SELECT pr.name,sum((pr.sp-pr.bp)* sl.quantity) as ttlprofit,sum(sl.quantity)as totalprofit FROM public.sales as sl join products as pr on pr.id=sl.product_id group by pr.name  ;")  
+    sales= cur.fetchall()
+    print(sales)
+    return render_template("viccistocksales.html",sale=sales,)
+
+@app.route('/sales/<int:id>')
+def sales(id):
+    cur=conn.cursor()
+
+    # cur.execute("SELECT pr.name,sum((pr.sp-pr.bp)* sl.quantity) as ttlprofit,sum(sl.quantity)as totalprofit FROM public.sales as sl join products as pr on pr.id=sl.product_id where id=%d group by pr.name ;",[id])
+    cur.execute("SELECT pr.name,sum((pr.sp-pr.bp)* sl.quantity) as ttlprofit,sum(sl.quantity)as totalprofit FROM public.sales as sl join products as pr on pr.id=sl.product_id where pr.id=%s group by pr.name ",[id])
+    sales= cur.fetchall()
+    print(sales)
+    return render_template("viccistocksales.html",sale=sales,)
+    
+app.run(debug=True)
+
