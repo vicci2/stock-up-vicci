a828fd5393d57464d4f0099f74ccca574f8f0aaa
Vicci <96298253+vicci2@users.noreply.github.com>
2022-04-21 19:22:01 +0300

    now

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app =Flask(__name__)
 app.secret_key="123secrete kye"
 
 try:
-    conn = psycopg2.connect("dbname='duka' user='postgres' host='localhost' password='vicciSQL'")
-    # conn = psycopg2.connect("dbname='d3a96tnlon0e19' user='zeuqvesoredycm' port='5432' host='ec2-52-48-159-67.eu-west-1.compute.amazonaws.com' password='f22ebebb9e3f5c8cb096b0fd0ed191458d3bd2703190f91c32c71d723dd7d586'")
-    # print ("Successfullly connected to the  Vicci database")
+    # conn = psycopg2.connect("dbname='duka' user='postgres' host='localhost' password='vicciSQL'")
+    conn = psycopg2.connect("dbname='d13aomlgi8qq6i' user='ewniyjtgheelry' port='5432' host='3b57e1fb10f3a1b2a37a8609cb1b0f661ce1023b3ab2af3c88d8788c56a3dee8' password='f22ebebb9e3f5c8cb096b0fd0ed191458d3bd2703190f91c32c71d723dd7d586'")
+    print ("Successfullly connected to the  Vicci database")
 except:
     print ("I am unable to connect to the  Vicci database")
 # cur=conn.cursor()
